3ad4e8bb03ba6274bf5a45b96cfb46954f1f9d42
21120036 <thienan2003bt@gmail.com>
2024-08-28 00:22:17 +0700

    Section 49: Handle DLX (dead letter exchange) message for notification microservice Section 48: nocode, learning Message Queue architecture

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const ConsumerQueueService = require('./src/services/consumerQueue.service');
 
 const queueName = 'test-topic';
 
-ConsumerQueueService.consumerToQueue(queueName).then(() => {
-    console.log("Message consumer started: " + queueName);
-}).catch(err => console.error("Message Queue server error: " + err))
+// ConsumerQueueService.consumerToQueue(queueName).then(() => {
+//     console.log("Message consumer started: " + queueName);
+// }).catch(err => console.error("Message Queue server error: " + err));
+
+ConsumerQueueService.consumerToQueueNormal().then(() => {
+    console.log("Message consumer normal started waiting for messages ...");
+}).catch(err => console.error("Message Queue server error: " + err));
+
+ConsumerQueueService.consumerToQueueFailed().then(() => {
+    console.log("Message consumer failed started waiting for messages ... ");
+}).catch(err => console.error("Message Queue server error: " + err));
--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -3,12 +3,56 @@
 const RabbitMQDatabase = require('../dbs/init.rabbitmq')
 
 class ConsumerQueueService {
+    static notiQueue = 'notificationQueueProcess';
+    static notiExchangeDLX = 'notificationExchangeDLX';
+    static notiRoutingKeyDLX = 'notificationRoutingKeyDLX';
+    static notiQueueHandler = 'notificationQueueHotFix';
+
     static async consumerToQueue(queueName) {
         try {
             const { channel, connection } = await RabbitMQDatabase.connectToRabbitMQ();
             await RabbitMQDatabase.consumeQueue(channel, queueName);
         } catch (error) {
             console.error("Error consumer to queue: ", error.message);
+        }
+    }
+
+    static async consumerToQueueNormal() {
+        try {
+            const { channel, connection } = await RabbitMQDatabase.connectToRabbitMQ();
+            await channel.assertQueue(ConsumerQueueService.notiQueue, {
+                exclusive: false,
+                durable: true,
+                deadLetterExchange: ConsumerQueueService.notiExchangeDLX,
+                deadLetterRoutingKey: ConsumerQueueService.notiRoutingKeyDLX
+            });
+
+            const ttl = 5000; //5s
+            setTimeout(() => {
+                channel.consume(ConsumerQueueService.notiQueue, (msg) => {
+                    console.log(`SEND notification successfully: ${msg?.content?.toString() ?? ''}`);
+                    channel.ack(msg);
+                })
+            }, ttl);
+        } catch (error) {
+            console.error("Error consumer to queue normal: ", error.message);
+        }
+    }
+
+    static async consumerToQueueFailed() {
+        try {
+            const { channel, connection } = await RabbitMQDatabase.connectToRabbitMQ();
+            await channel.assertExchange(ConsumerQueueService.notiExchangeDLX, 'direct', { durable: true });
+            const queueResult = await channel.assertQueue(ConsumerQueueService.notiQueueHandler, { exclusive: false });
+
+            await channel.bindQueue(queueResult.queue, ConsumerQueueService.notiExchangeDLX, ConsumerQueueService.notiRoutingKeyDLX);
+
+            channel.consume(queueResult.queue, (msgFailed) => {
+                console.log(`Error notification, please hot fix it: ${msgFailed?.content?.toString() ?? ''}`);
+            }, { noAck: true })
+        } catch (error) {
+            console.error("Error consumer to queue failed: ", error.message);
+            throw error;
         }
     }
 }
